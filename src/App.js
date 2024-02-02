312d6f9f15e56460d5ab3e758b2928559247310d
Nikhilkarn02 <155177972+Nikhilkarn02@users.noreply.github.com>
2024-02-02 14:08:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=864ac175';
+const API_URL = 'https://www.omdbapi.com?apikey=864ac175';
 
 
 const App = () => {
